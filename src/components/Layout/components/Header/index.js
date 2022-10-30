ba359dfeb6307a4fd3f3f0ebfc0f35a9ec503350
SonThee <theson2604@gmail.com>
2022-10-30 14:54:07 +0700

    add logo

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,10 +1,15 @@
 import styles from "./Header.module.scss";
 import clsx from "clsx";
+import images from "~/assets/images";
 
 function Header() {
   return (
     <header className={clsx(styles.wrapper)}>
-      <div className={clsx(styles.inner)}></div>
+      <div className={clsx(styles.inner)}>
+        <div className={clsx(styles.logo)}>
+          <img src={images.logo} alt="logo" />
+        </div>
+      </div>
     </header>
   );
 }
